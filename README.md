5282c40046094b254c1a8621df552628820721b2
ANDgamer <andriychernysh88@gmail.com>
2023-07-27 03:59:51 +0300

    Changed ALL -_-

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Labyrinth_bot - This is an algorithm that finds a way through almost any maze presented in the form of a data array.
+
+Use ".json" or ".m" files ("first" or "second" method respectively in "Script.m"
+
 ! The path may not be the shortest !
--- Script.m
+++ Script.m
@@ -2,50 +2,62 @@
 clc; 
 clear all;
 close all; 
-run('Labyrinth.m');
 
-main_massive = MyLabyrinth;
-final_massive = MyLabyrinth;
+%The first method
+    fname = 'maze.json'; 
+    fid = fopen(fname); 
+    raw = fread(fid,inf); 
+    str = char(raw'); 
+    fclose(fid); 
+    main_massive = jsondecode(str);
+    final_massive = jsondecode(str);
+%end
+
+%second method
+    %run('Data.m');
+    %main_massive = Labyrinth;
+    %final_massive = Labyrinth;
+%end
 
 program_active = true;
 
 n = 2;
 
-j = 7;
-i = 2;
+j = 6;
+i = 1;
 
-data = cell(2, 99);
+data = zeros(2, 999);
 
-data{1, 1} = j;
-data{2, 1} = i;
+data(1, 1) = j;
+data(2, 1) = i;
 fprintf("n = %d j = %d i = %d \n", 1, j, i);
-main_massive{j, i} = '0';
+main_massive(j, i) = 8;
 
 while program_active == true
-    if main_massive{j-1, i} == ' '
+    if main_massive(j-1, i) == 0
         pzdc = false;
         j = j-1;
-    elseif main_massive{j, i+1} == ' '
+    elseif main_massive(j, i+1) == 0
         pzdc = false;
         i = i+1;
-    elseif main_massive{j+1, i} == ' '
+    elseif main_massive(j+1, i) == 0
         pzdc = false;
         j = j+1;
-    elseif main_massive{j, i-1} == ' '
+    elseif main_massive(j, i-1) == 0
         pzdc = false;
         i = i-1;
     else
         disp("HZ");
         pzdc = true;
         n = n - 1;
-        j = data{1, n-1};
-        i = data{2, n-1};
-        main_massive{j, i} = '0';
+        j = data(1, n-1);
+        i = data(2, n-1);
+        main_massive(j, i) = 8;
     end
     
-    main_massive{j, i} = '0';
-    data{1, n} = j;
-    data{2, n} = i;
+    main_massive(j, i) = 8;
+    data(1, n) = j;
+    data(2, n) = i;
     fprintf("n = %d j = %d i = %d \n", n, j, i);
     
     if j == size(main_massive, 1) || i == size(main_massive, 2)
@@ -60,11 +72,11 @@ while program_active == true
 end
 
 figure(1)
-cellplot(main_massive);
+imagesc(main_massive);
 
 for x = 1:n
-    final_massive{data{1, x}, data{2, x}} = '0';
+    final_massive(data(1, x), data(2, x)) = 8;
 end
 
 figure(2)
-cellplot(final_massive);
+imagesc(final_massive);
